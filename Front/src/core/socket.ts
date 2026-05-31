c7e02f3c2522e7983818cff5b18bdd87321926a6
KazuDolfo <iradolfo640@gmail.com>
2026-05-31 17:56:26 -0500

    Update backend URL for socket connection

--- Front/src/core/socket.ts
+++ Front/src/core/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from 'socket.io-client';
 
 // URL del backend desplegado en Render
-const BACKEND_URL = 'https://virus-backend-8nvg.onrender.com';
+const BACKEND_URL = 'https://virus-backend01.onrender.com';
 
 export const socket: Socket = io(BACKEND_URL, {
   autoConnect: true,
